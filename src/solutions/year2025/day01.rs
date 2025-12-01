083ed7dea688235a711f0b334da17594307571ec
Utsob Roy <roy@utsob.me>
2025-12-01 19:10:58 +0600

    perf: simplify pipeline

--- src/solutions/year2025/day01.rs
+++ src/solutions/year2025/day01.rs
@@ -27,42 +27,37 @@ fn get_dial_position(current_pos: i32, offset: i32, dial_size: i32) -> (i32, i32
 const DIAL_SIZE: i32 = 100;
 
 pub fn solution_2025_01_01(filepath: String) -> Option<i32> {
-    let mut current_position = 50;
-    let mut zero_count = 0;
-    for ins in std::fs::read_to_string(filepath)
+    let (_, zero_count) = std::fs::read_to_string(filepath)
         .expect("Invalid file")
         .trim_end()
         .lines()
-    {
-        let ins = parse_instruction(ins);
-        (current_position, _) = get_dial_position(current_position, ins, DIAL_SIZE);
-        if current_position == 0 {
-            zero_count += 1;
-        }
-    }
+        .map(parse_instruction)
+        .fold((50, 0), |(current_position, zero_count), ins| {
+            let (new_position, _) = get_dial_position(current_position, ins, DIAL_SIZE);
+            let zero_count = if new_position == 0 {
+                zero_count + 1
+            } else {
+                zero_count
+            };
+            (new_position, zero_count)
+        });
     Some(zero_count)
 }
 
 pub fn solution_2025_01_02(filepath: String) -> Option<i32> {
-    let mut current_position = 50;
-    let mut zero_count = 0;
-    for ins in std::fs::read_to_string(filepath)
+    let (_, zero_count) = std::fs::read_to_string(filepath)
         .expect("Invalid file")
         .trim_end()
         .lines()
-    {
-        let ins = parse_instruction(ins);
-        let (cur_pos, rotations) = get_dial_position(current_position, ins, DIAL_SIZE);
-        println!(
-            "Instruction: {:?}, New Position: {}, rotations: {}",
-            ins, cur_pos, rotations
-        );
-        current_position = cur_pos;
-        zero_count += rotations;
-        if current_position == 0 && rotations == 0 {
-            zero_count += 1;
-        }
-    }
+        .map(parse_instruction)
+        .fold((50, 0), |(current_position, zero_count), ins| {
+            let (new_position, rotations) = get_dial_position(current_position, ins, DIAL_SIZE);
+            let mut zero_count = zero_count + rotations;
+            if new_position == 0 && rotations == 0 {
+                zero_count += 1;
+            }
+            (new_position, zero_count)
+        });
     Some(zero_count)
 }
 
